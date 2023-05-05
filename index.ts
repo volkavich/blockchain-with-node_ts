add814914a68b5ba3db5900576ca30d05d099dbb
Rahman <abdulrahman6093@outlook.com>
2023-05-05 12:41:56 +0530

    formatted with prettier

--- index.ts
+++ index.ts
@@ -1,119 +1,116 @@
 import * as crypto from "crypto";
 
-class Transaction{
-    constructor(
-        public amount : number,
-        public payer : string, //public key
-        public payee : string //public key
-    ){}
+class Transaction {
+  constructor(
+    public amount: number,
+    public payer: string, //public key
+    public payee: string //public key
+  ) {}
 
-    toString(){
-        return JSON.stringify(this);
-    }
+  toString() {
+    return JSON.stringify(this);
+  }
 }
 
-class Block{
+class Block {
+  public nonce = Math.round(Math.random() * 999999999);
 
-    public nonce = Math.round(Math.random() * 999999999);
+  constructor(
+    public prevHash: string,
+    public transaction: Transaction,
+    public ts = Date.now()
+  ) {}
 
-    constructor(
-        public prevHash : string,
-        public transaction : Transaction,
-        public ts = Date.now()
-    ){}
-
-    get hash(){
-        const str = JSON.stringify(this);
-        const hash = crypto.createHash("SHA256");
-        hash.update(str).end();
-        return hash.digest("hex");
-    }
-
+  get hash() {
+    const str = JSON.stringify(this);
+    const hash = crypto.createHash("SHA256");
+    hash.update(str).end();
+    return hash.digest("hex");
+  }
 }
 
+class Chain {
+  public static instance = new Chain();
 
-class Chain{
-    public static instance = new Chain();
+  chain: Block[];
 
-    chain : Block[];
+  constructor() {
+    this.chain = [new Block("", new Transaction(100, "genesis", "satoshi"))];
+  }
 
-    constructor(){
-        this.chain = [new Block("", new Transaction(100,"genesis", "satoshi"))];
+  get lastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  mine(nonce: number) {
+    let solution = 1;
+    console.log("Mining....");
+
+    while (true) {
+      const hash = crypto.createHash("MD5"); //used MD5 --> faster to compute than SHA256
+      hash.update((nonce + solution).toString()).end;
+
+      const attempt = hash.digest("hex");
+
+      if (attempt.substr(0, 4) === "0000") {
+        console.log(`Solved: ${solution}`);
+        return solution;
+      }
+
+      solution += 1;
     }
+  }
 
-    get lastBlock(){
-        return this.chain[this.chain.length - 1];
+  addBlock(
+    transaction: Transaction,
+    senderPublicKey: string,
+    signature: Buffer
+  ) {
+    // const newBlock = new Block(this.lastBlock.hash, transaction);
+    // this.chain.push(newBlock);
+
+    const verifier = crypto.createVerify("SHA256");
+    verifier.update(transaction.toString());
+
+    const isValid = verifier.verify(senderPublicKey, signature);
+
+    if (isValid) {
+      const newBlock = new Block(this.lastBlock.hash, transaction);
+      this.mine(newBlock.nonce);
+      this.chain.push(newBlock);
     }
-
-    mine(nonce:number){
-        let solution = 1;
-        console.log("Mining....")
-
-        while(true){
-            const hash = crypto.createHash("MD5");  //used MD5 --> faster to compute than SHA256
-            hash.update((nonce + solution).toString()).end;
-
-            const attempt = hash.digest("hex");
-
-            if(attempt.substr(0,4) === "0000"){
-                console.log(`Solved: ${solution}`);
-                return solution;
-            }
-
-            solution += 1;
-        }
-    }
-
-    addBlock(transaction: Transaction, senderPublicKey: string, signature: Buffer){
-        // const newBlock = new Block(this.lastBlock.hash, transaction);
-        // this.chain.push(newBlock); 
-
-        const verifier = crypto.createVerify("SHA256");
-        verifier.update(transaction.toString());
-
-        const isValid = verifier.verify(senderPublicKey, signature);
-
-        if(isValid){
-            const newBlock = new Block(this.lastBlock.hash, transaction);
-            this.mine(newBlock.nonce); 
-            this.chain.push(newBlock);
-        }
-    }
+  }
 }
 
+class Wallet {
+  public publicKey: string;
+  public privateKey: string;
 
+  constructor() {
+    const keypair = crypto.generateKeyPairSync("rsa", {
+      modulusLength: 2048,
+      publicKeyEncoding: { type: "spki", format: "pem" },
+      privateKeyEncoding: { type: "pkcs8", format: "pem" },
+    });
 
-class Wallet{
+    this.privateKey = keypair.privateKey;
+    this.publicKey = keypair.publicKey;
+  }
 
-    public publicKey: string;
-    public privateKey: string;
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
 
-    constructor() {
-        const keypair = crypto.generateKeyPairSync('rsa', {
-          modulusLength: 2048,
-          publicKeyEncoding: { type: 'spki', format: 'pem' },
-          privateKeyEncoding: { type: 'pkcs8', format: 'pem' },
-        });
-    
-        this.privateKey = keypair.privateKey;
-        this.publicKey = keypair.publicKey;
-    }
+    const sign = crypto.createSign("SHA256");
+    sign.update(transaction.toString()).end();
 
-    sendMoney(amount:number, payeePublicKey: string){
-         const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
-
-         const sign  = crypto.createSign("SHA256");
-         sign.update(transaction.toString()).end();
-         
-         const signature = sign.sign(this.privateKey);
-         Chain.instance.addBlock(transaction, this.publicKey, signature);
-    }
+    const signature = sign.sign(this.privateKey);
+    Chain.instance.addBlock(transaction, this.publicKey, signature);
+  }
 }
-
 
 const mohan = new Wallet();
 const ranga = new Wallet();
-const satoshi  = new Wallet();
+const satoshi = new Wallet();
 
 satoshi.sendMoney(50, mohan.publicKey);
 mohan.sendMoney(40, ranga.publicKey);
